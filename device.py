c427876a5fda264bdb1a1af04cb6d8521ddbec8c
Amirhesam Abedsoltan <91305454+ahabedsoltan@users.noreply.github.com>
2023-08-07 21:31:28 -0700

    Update device.py
    input/output hints added, order of methods modified

--- device.py
+++ device.py
@@ -1,27 +1,25 @@
 import torch
 from torch.cuda.comm import broadcast
-
+from typing import List, Union
 
 class Device():
     """Handles tensor operations across multiple devices."""
 
-    def __init__(self, devices):
-        """Initializes the Device object with available devices.
-
-        Args:
-            devices (list): List of available devices.
-        """
+    def __init__(self, devices: List[torch.device]):
+        """Initializes the Device object with available devices."""
         self.devices = devices
 
-    def distribute_tensor_across_gpus(self, tensor):
-        """Divides a tensor evenly across multiple GPUs.
+    def __call__(self, tensor: torch.Tensor, strategy: str = "broadcast") -> Union[torch.Tensor, List[torch.Tensor]]:
+        """Applies a distribution strategy to a tensor."""
+        if strategy == "divide_to_gpu":
+            return self.distribute_tensor_across_gpus(tensor)
+        elif strategy == "broadcast":
+            return broadcast(tensor, self.devices)
+        elif strategy == "base":
+            return tensor.to(self.devices[0])
 
-        Args:
-            tensor (torch.Tensor): Tensor to distribute.
-
-        Returns:
-            list: List of tensor segments, each located on a different device.
-        """
+    def distribute_tensor_across_gpus(self, tensor: torch.Tensor) -> List[torch.Tensor]:
+        """Divides a tensor evenly across multiple GPUs."""
         tensor_segments = []
         segment_size = tensor.shape[0] // len(self.devices)
         for i in range(len(self.devices)):
@@ -30,36 +28,13 @@ class Device():
             else:
                 tensor_slice = tensor[i * segment_size:, :]
             tensor_segments.append(tensor_slice.to(self.devices[i]))
-
         return tensor_segments
 
-    def __call__(self, tensor, strategy="broadcast"):
-        """Applies a distribution strategy to a tensor.
-
-        Args:
-            tensor (torch.Tensor): Tensor to distribute.
-            strategy (str, optional): Distribution strategy. Defaults to "broadcast".
-
-        Returns:
-            list or torch.Tensor: Result of applying the distribution strategy to the tensor.
-        """
-        if strategy == "divide_to_gpu":
-            return self.distribute_tensor_across_gpus(tensor)
-        elif strategy == "broadcast":
-            return broadcast(tensor, self.devices)
-        elif strategy == "base":
-            return tensor.to(self.devices[0])
-
     @staticmethod
-    def create():
-        """Creates a Device object representing all available GPUs or CPU if no GPUs are available.
-
-        Returns:
-            Device: Device object with all available GPUs or CPU.
-        """
+    def create() -> 'Device':
+        """Creates a Device object representing all available GPUs or CPU if no GPUs are available."""
         if torch.cuda.is_available():
             device_list = [torch.device(f'cuda:{i}') for i in range(torch.cuda.device_count())]
         else:
             device_list = [torch.device('cpu')]
-
         return Device(device_list)
