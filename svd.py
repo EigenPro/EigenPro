3faad330f4abedc547c969161e2fb577efbeecb9
Amirhesam Abedsoltan <91305454+ahabedsoltan@users.noreply.github.com>
2023-08-13 13:33:59 -0700

    Update svd.py

--- svd.py
+++ svd.py
@@ -1,35 +1,31 @@
 '''Utility functions for performing fast SVD.'''
 
-import scipy.linalg as linalg
 import numpy as np
+import scipy.linalg as linalg
 
 
-def top_q_eig(matrix, top_q):
+def top_q_eig(matrix: np.ndarray, q: int) -> (np.ndarray, np.ndarray, float):
     """
-    Find the top q eigenvalues and eigenvectors of a matrix using torch.lobpcg.
+    Find the top q eigenvalues and eigenvectors of a matrix using scipy.linalg.eigh.
 
     Args:
-    - matrix (torch.Tensor): Symmetric matrix of shape (n, n).
+    - matrix (np.ndarray): Symmetric matrix of shape (n, n).
     - q (int): Number of top eigenvalues/eigenvectors to retrieve.
 
     Returns:
-    - eigenvalues (torch.Tensor): Tensor of shape (q,) containing top q eigenvalues.
-    - eigenvectors (torch.Tensor): Tensor of shape (n, q) containing corresponding eigenvectors.
+    - eigenvalues (np.ndarray): Array of shape (q,) containing top q eigenvalues.
+    - eigenvectors (np.ndarray): Array of shape (n, q) containing corresponding eigenvectors.
+    - next_eigenvalue (float): The (q+1)-th eigenvalue.
     """
-
-    # Ensure the matrix is symmetric and in numpy.
+    
     if not isinstance(matrix, np.ndarray):
-        matrix =  np.array(matrix)
+        matrix = np.array(matrix)
+    
     assert matrix.shape[0] == matrix.shape[1], "Matrix should be square."
-
+    
     n_sample = matrix.shape[0]
+    eigenvalues, eigenvectors = linalg.eigh(matrix, subset_by_index=[n_sample - q - 1, n_sample - 1])
+    eigenvalues = eigenvalues[::-1]
+    eigenvectors = eigenvectors[:, ::-1]
 
-    # Find eigenvalues and eigenvectors.
-    # eigenvalues, eigenvectors = torch.lobpcg(matrix,k=q+1)
-    eigenvalues, eigenvectors = linalg.eigh( matrix,
-                                             subset_by_index = [n_sample - top_q - 1, n_sample - 1],eigvals_only=False )
-    eigenvalues  = eigenvalues[::-1]
-    eigenvectors = eigenvectors[:,::-1]
-
-
-    return eigenvalues[:top_q], eigenvectors[:,:top_q],eigenvalues[top_q]
+    return eigenvalues[:q], eigenvectors[:, :q], eigenvalues[q]
