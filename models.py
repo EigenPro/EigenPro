5f2410fea48b49c2efc72cecbc4db932555bd8d9
Amirhesam Abedsoltan <amirhesam@exp-7-59.expanse.sdsc.edu>
2023-08-25 09:55:05 -0700

    eliminated device from ShardedKernelMachine class

--- models.py
+++ models.py
@@ -1,104 +1,146 @@
 """Classes of Kernel Machines."""
 
-from typing import Callable, Optional
+from typing import Callable, Optional, List
 import torch
+from concurrent.futures import ThreadPoolExecutor
+from device import Device
+
+import ipdb
 
 
-class KernelMachine(object):
-  """Kernel machine class for handling kernel methods.
-  
-  Provides methods for creating and manipulating kernel machines including 
-  adding centers, computing the forward pass, and copying the current 
-  instance.
+class KernelMachine:
+    """Base class for KernelMachine."""
 
-  Attributes:
-    n_outputs: The number of outputs.
-    _kernel_fn: A callable that computes the kernel function.
-    _center_blocks: A list of tensors of kernel centers.
-    _weight_blocks: A list of trainable tensors of weights.
-  """
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        raise NotImplementedError
 
-  def __init__(self, 
-               kernel_fn: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
-               n_outputs: int, 
-               centers: torch.Tensor,
-               weights: Optional[torch.Tensor] = None) -> None:
-    """Initializes a new KernelMachine instance.
+    def add_centers(self, centers: torch.Tensor, weights: Optional[torch.Tensor] = None) -> torch.Tensor:
+        raise NotImplementedError
 
-    Args:
-      kernel_fn: A callable that computes the kernel function.
+
+class PreallocatedKernelMachine(KernelMachine):
+    """Kernel machine class for handling kernel methods.
+
+    Attributes:
       n_outputs: The number of outputs.
-      centers: A tensor of kernel centers of shape [n_centers, n_features].
-      weights: An optional tensor of weights of shape [n_centers, n_outputs].
+      _kernel_fn: A callable that computes the kernel function.
+      _center_blocks: A list of tensors of kernel centers.
+      _weight_blocks: A list of trainable tensors of weights.
     """
-    self._kernel_fn = kernel_fn
-    self._n_outputs = n_outputs
-    self._center_blocks = []
-    self._weight_blocks = []
-    _ = self.add_centers(centers, weights)
-  
-  @property
-  def n_outputs(self) -> int:
-    """Returns the number of outputs."""
-    return self._n_outputs
-  
-  @property
-  def n_centers(self) -> int:
-    """Return the number of centers."""
-    count = 0
-    for center_block in self._center_blocks:
-      count += center_block.size(dim=0)
-    return count
-  
-  def forward(self, x: torch.Tensor) -> torch.Tensor:
-    """Forward pass of the kernel model.
-    
-    Args:
-      x: Input mini-batch tensor of shape [batch_size, n_features].
 
-    Returns:
-      Output tensor of shape [batch_size, n_outputs].
-    """
-    # Center matrix of shape [n_centers, n_features].
-    centers = torch.cat(self._center_blocks, dim=0)
-    # Kernel matrix of shape [batch_size, n_centers].
-    kernel_mat = self._kernel_fn(x, centers)
-    # Weight matrix of shape [n_centers, n_outputs].
-    weights = torch.cat(self._weight_blocks, dim=0)
-    # Output of shape [batch_size, n_outputs]
-    output = kernel_mat.matmul(weights)
-    return output
-  
-  def add_centers(self, 
-                  centers: torch.Tensor,
-                  weights: Optional[torch.Tensor] = None) -> torch.Tensor:
-    """Adds new centers and weights.
-    
-    Args:
-      centers: Kernel centers of shape [n_centers, n_features].
-      weights: Weight parameters corresponding to the centers of shape 
-               [n_centers, n_output].
+    def __init__(self,
+                 kernel_fn: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
+                 n_outputs: int,
+                 centers: torch.Tensor,
+                 preallocation_size: Optional[int] = None,
+                 weights: Optional[torch.Tensor] = None,
+                 device: Optional[torch.device] = 'cpu') -> None:
+        self.device = device
+        self._kernel_fn = kernel_fn
+        self._n_outputs = n_outputs
+        self.original_size = centers.shape[0]
+        if preallocation_size is None:
+            self._centers = torch.zeros(2 * centers.shape[0], centers.shape[1], device=device)
+            self._weights = torch.zeros(2 * centers.shape[0], self._n_outputs, device=device)
+        else:
+            self._centers = torch.zeros(preallocation_size, centers.shape[1], device=device)
+            self._weights = torch.zeros(preallocation_size, self._n_outputs, device=device)
+        self.used_capacity = 0
+        self.add_centers(centers, weights)
 
-    Returns:
-      center_weights: Weight parameters corresponding to the added centers.
-    """
-    self._center_blocks.append(centers)
-    # TODO: Check if the number of rows of centers equals that of weights.
-    center_weights = weights if weights is not None else torch.zeros(
-        (centers.shape[0], self.n_outputs))
-    self._weight_blocks.append(center_weights)
-    return center_weights
-  
-  def shallow_copy(self) -> 'KernelMachine':
-    """Creates a shallow copy of the current kernel machine.
+    @property
+    def n_outputs(self) -> int:
+        """Returns the number of outputs."""
+        return self._n_outputs
 
-    Returns:
-      A new KernelMachine instance that shares the same underlying centers and 
-      weights.
+    @property
+    def n_centers(self) -> int:
+        """Return the number of centers."""
+        return self.used_capacity
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = x.to(self.device)
+        kernel_mat = self._kernel_fn(x, self._centers[:self.used_capacity, :])
+        weights = self._weights[:self.used_capacity, :]
+        return kernel_mat @ weights
+
+    def add_centers(self, centers: torch.Tensor, weights: Optional[torch.Tensor] = None) -> torch.Tensor:
+        centers = centers.to(self.device)
+        if weights is not None:
+            weights = weights.to(self.device)
+        else:
+            weights = torch.zeros((centers.shape[0], self.n_outputs), device=self.device)
+
+        self._centers[self.used_capacity:self.used_capacity + centers.shape[0], :] = centers
+        self._weights[self.used_capacity:self.used_capacity + centers.shape[0], :] = weights
+        self.used_capacity += centers.shape[0]
+
+        if self.used_capacity > 2 * self.original_size:
+            self._centers[self.used_capacity:, :] = 0
+            self._weights[self.used_capacity:, :] = 0
+            self.used_capacity = self.original_size
+
+        return weights
+
+
+class BlockKernelMachine(KernelMachine):
+    """Kernel machine class for handling kernel methods using block storage.
+
+    Attributes:
+      n_outputs: The number of outputs.
+      _kernel_fn: A callable that computes the kernel function.
+      _center_blocks: A list of tensors of kernel centers.
+      _weight_blocks: A list of trainable tensors of weights.
     """
-    copy = KernelMachine(self._kernel_fn, self.n_outputs,
-                         self._center_blocks[0], self._weight_blocks[0])
-    for center_block, weight_block in zip(self._center_blocks[1:],
-                                          self._weight_blocks[1:]):
-      copy.add_centers(center_block, weight_block)
-    return copy
+
+    def __init__(self,
+                 kernel_fn: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
+                 n_outputs: int,
+                 centers: torch.Tensor,
+                 weights: Optional[torch.Tensor] = None) -> None:
+        self._kernel_fn = kernel_fn
+        self._n_outputs = n_outputs
+        self._center_blocks = [centers]
+        self._weight_blocks = [weights if weights is not None else torch.zeros(centers.shape[0], n_outputs)]
+
+    @property
+    def n_outputs(self) -> int:
+        return self._n_outputs
+
+    @property
+    def n_centers(self) -> int:
+        return sum(center_block.size(0) for center_block in self._center_blocks)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        centers = torch.cat(self._center_blocks, dim=0)
+        kernel_mat = self._kernel_fn(x, centers)
+        weights = torch.cat(self._weight_blocks, dim=0)
+        return kernel_mat @ weights
+
+    def add_centers(self, centers: torch.Tensor, weights: Optional[torch.Tensor] = None) -> torch.Tensor:
+        if weights is None:
+            weights = torch.zeros(centers.shape[0], self.n_outputs)
+        self._center_blocks.append(centers)
+        self._weight_blocks.append(weights)
+        return weights
+
+    def shallow_copy(self) -> 'KernelMachine':
+        copy = BlockKernelMachine(self._kernel_fn, self.n_outputs, self._center_blocks[0], self._weight_blocks[0])
+        for center_block, weight_block in zip(self._center_blocks[1:], self._weight_blocks[1:]):
+            copy.add_centers(center_block, weight_block)
+        return copy
+
+class ShardedKernelMachine(KernelMachine):
+    """Kernel machine that shards its computation across multiple devices."""
+    def __init__(self, kms: List[PreallocatedKernelMachine]):
+        self.shard_kms = kms
+        self.n_machines = len(kms)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        with ThreadPoolExecutor() as executor:
+            kernel_matrices = [executor.submit(self.shard_kms[i].forward, x)
+                               for i in range(self.n_machines)]
+        return [k.result() for k in kernel_matrices]
+    def add_centers(self, centers_list: torch.Tensor) -> None:
+        with ThreadPoolExecutor() as executor:
+            _ = [executor.submit(self.shard_kms[i].add_centers, centers_list[i]) for
+                 i in range(self.n_machines)]
--- models_test.py
+++ models_test.py
@@ -1,6 +1,9 @@
 import unittest
 import torch
-from models import KernelMachine
+from models import ShardedKernelMachine, PreallocatedKernelMachine
+from device import Device
+
+import ipdb
 
 def linear_kernel(x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
     return x1.matmul(x2.t())
@@ -10,33 +13,65 @@ class TestKernelMachine(unittest.TestCase):
     def setUp(self):
         self.kernel_fn = linear_kernel
         self.n_outputs = 2
-        self.centers = torch.tensor([[1.0, 2.0], [3.0, 4.0]])
-        self.weights = torch.tensor([[1.0, 0.5], [0.5, 1.0]])
-        self.machine = KernelMachine(self.kernel_fn, self.n_outputs, self.centers, self.weights)
 
+        self.device = Device.create()
+        self.kms = []
+
+
+
+        for d in self.device.devices:
+          centers = torch.tensor([[1.0, 2.0], [3.0, 4.0], [5., 6.0]])
+          weights = torch.tensor([[1.0, 0.5], [0.5, 1.0], [0.5, 0.5]])
+          self.kms.append( PreallocatedKernelMachine(self.kernel_fn, self.n_outputs ,centers,weights=weights,device= d) )
+
+
+        self.ShardedKernelMachine = ShardedKernelMachine(self.kms)
+
+        centers = torch.tensor([[1.0, 2.0], [3.0, 4.0], [5., 6.0]])
+        weights = torch.tensor([[1.0, 0.5], [0.5, 1.0], [0.5, 0.5]])
+        self.km_smaple = PreallocatedKernelMachine(self.kernel_fn, self.n_outputs, centers, weights=weights)
+
+    def are_tensor_lists_equal(self,list1, list2):
+        if len(list1) != len(list2):
+            return False
+        for tensor1, tensor2 in zip(list1, list2):
+            if not torch.equal(tensor1, tensor2):
+                return False
+        return True
+    ####checking PreallocatedKernelMachine
     def test_n_outputs(self):
-        self.assertEqual(self.machine.n_outputs, self.n_outputs)
+        self.assertEqual(self.km_smaple.n_outputs, self.n_outputs)
 
     def test_n_centers(self):
-        self.assertEqual(self.machine.n_centers, 2)
+        self.assertEqual(self.km_smaple.n_centers, 3)
 
     def test_forward(self):
         x = torch.tensor([[1.0, 1.0]])
-        expected_output = torch.tensor([[6.5, 8.5]])
-        torch.testing.assert_close(self.machine.forward(x), expected_output)
+        expected_output = torch.tensor([[12., 14.]])
+        torch.testing.assert_close(self.km_smaple.forward(x), expected_output)
 
     def test_add_centers(self):
         new_centers = torch.tensor([[5.0, 6.0]])
         new_weights = torch.tensor([[0.5, 0.5]])
-        self.machine.add_centers(new_centers, new_weights)
-        self.assertEqual(self.machine.n_centers, 3)
+        self.km_smaple.add_centers(new_centers, new_weights)
+        self.assertEqual(self.km_smaple.n_centers, 4)
 
-    def test_shallow_copy(self):
-        copy_machine = self.machine.shallow_copy()
-        self.assertEqual(copy_machine.n_outputs, self.machine.n_outputs)
-        self.assertEqual(copy_machine.n_centers, self.machine.n_centers)
-        torch.testing.assert_close(copy_machine.forward(torch.tensor([[1.0, 1.0]])), 
-                                      self.machine.forward(torch.tensor([[1.0, 1.0]])))
+    ####checking ShardedKernelMachine
+    def test_forward_sharded(self):
+        x = torch.tensor([[1.0, 1.0]])
+        out = self.ShardedKernelMachine.forward(x)
+        expected_output = []
+        for d in self.device.devices:
+          expected_output.append(torch.tensor([[12, 14]]).to(d))
+        self.are_tensor_lists_equal(out, expected_output)
+    def test_add_centers_sharded(self):
+        new_centers = torch.tensor([[7.0, 8.0]])
+        new_centers_list = self.device(new_centers,strategy = "divide_to_gpu")
+        n_devices = len(self.device.devices)
+        self.ShardedKernelMachine.add_centers(new_centers_list)
+        for i,_ in enumerate(self.device.devices):
+          self.assertEqual(self.ShardedKernelMachine.shard_kms[i].n_centers, 3+(i+1)//n_devices)
+
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(verbosity=2)
