2247637d640286cad2af2d467839f3692392323b
Siyuan <siyuan.ma9@gmail.com>
2023-08-27 20:21:21 -0700

    Add Preconditioner class

---
+++ preconditioner.py
@@ -0,0 +1,153 @@
+"""Preconditioner class and utility functions for EigenPro iteration.
+"""
+from typing import Callable
+import numpy as np
+import torch
+
+import svd
+
+
+class KernelEigenSystem(svd.EigenSystem):
+    """Extends the EigenSystem class for EigenPro kernel preconditioning.
+    
+    Attributes:
+        _beta (float): (Approximate) maxinum of kernel norm.
+        _scale (float): Scale factor.
+    """
+    
+    def __init__(self, eigensys: svd.EigenSystem, beta: float, scale: float
+                 ) -> None:
+        """Initialize an KernelEigenSystem instance.
+        
+        Args:
+            eigensys (svd.EigenSystem): An instance of EigenSystem to extend.
+            beta (float): (Approximate) maxinum of kernel norm.
+            scale (float): The scale factor.
+        """
+        self.__dict__ = eigensys.__dict__
+        self._beta = beta
+        self._scale = scale
+        # Overwrites base class `_vectors`
+        self._vectors = torch.tensor(self.vectors.copy(),
+                                     dtype=torch.float32)
+        self._normalize_ratios = torch.tensor(
+            self._scale * (1 - self.min_value / self.values) / self.values)
+
+    @property
+    def beta(self) -> float:
+        """Get the beta parameter.
+
+        Returns:
+            float: The beta parameter.
+        """
+        return self._beta
+
+    @property
+    def normalized_ratios(self) -> np.ndarray:
+        """Calculates the normalized eigenvalue ratio for kernel operator.
+
+        Returns:
+            np.ndarray: The computed ratios.
+        """
+        return self._normalize_ratios
+
+
+
+def top_eigensystem(samples: torch.Tensor, top_q: int,
+                    kernel_fn: Callable[[torch.Tensor, torch.Tensor],
+                                        torch.Tensor]
+                    ) -> KernelEigenSystem:
+    """Computes the top eigen system for a kernel matrix.
+
+    Args:
+        samples (torch.Tensor): A tensor containing the samples.
+        top_q (int): The number of top eigenvalues to consider from the
+            eigenspectrum.
+        kernel_fn (Callable[[torch.Tensor, torch.Tensor], torch.Tensor]): 
+            A kernel function that computes the kernel matrix for two sets of
+            samples.
+
+    Returns:
+        A KernelEigenSystem instance
+
+    """
+    n_sample = samples.shape[0]
+    kernel_mat = kernel_fn(samples, samples).cpu().data.numpy()
+    eigensys = svd.top_q_eig(kernel_mat, top_q)
+    # Obtains an upper bound for ||k(x, \cdot)||.
+    beta = max(np.diag(kernel_mat))
+
+    n_samples = samples.shape[0]
+    return KernelEigenSystem(eigensys, beta, 1 / n_sample)
+
+
+class Preconditioner:
+    """Class for preconditioning based on a given kernel function and centers.
+
+    Attributes:
+        kernel_fn: Callable kernel function that takes centers and inputs,
+            and returns a kernel matrix.
+        centers: Tensor representing kernel centers of shape [n_centers,
+            n_features].
+        weights: Weight parameters of shape [n_centers, n_outputs] corresponding
+            to the centers.
+        top_q_eig: Construct top q eigensystem for preconditioning.
+    """
+    
+    def __init__(self, 
+                 kernel_fn: Callable[[torch.Tensor, torch.Tensor], torch.Tensor], 
+                 centers: torch.Tensor, 
+                 weights: torch.Tensor, 
+                 top_q_eig: int) -> None:
+        """Initializes the Preconditioner."""
+        self._kernel_fn = kernel_fn
+        self._centers = centers
+        self._weights = weights
+        self._eigensys = top_eigensystem(centers, top_q_eig, kernel_fn)
+        
+    @property
+    def critical_batch_size(self) -> int:
+        """Computes and returns the critical batch size."""
+        return int(self._eigensys.beta / self._eigensys.min_value)
+    
+    def learning_rate(self, batch_size: int) -> float:
+        """Computes and returns the learning rate based on the batch size."""
+        if batch_size < self.critical_batch_size:
+            return batch_size / self._eigensys.beta / 2
+        else:
+            return batch_size / (self._eigensys.beta + 
+                                 (batch_size - 1) * self._eigensys.min_value)
+            
+    def scaled_learning_rate(self, batch_size: int) -> float:
+        """Computes and returns the scaled learning rate."""
+        return 2 / batch_size * self.learning_rate(batch_size)
+    
+    def delta(self, batch_x: torch.Tensor, grad: torch.Tensor) -> torch.Tensor:
+        """Computes weight delta for preconditioner centers.
+        Args:
+            batch_x (torch.Tensor): Of shape `[batch_size, n_features]`.
+            grad (torch.Tensor): Of shape `[batch_size, n_outputs]`.
+
+        Returns:
+            torch.Tensor: Of Shape `[n_centers, n_outputs]`.
+
+        Raises:
+            None: This method is not expected to raise any exceptions.
+        """
+        kernel_mat = self._kernel_fn(self._centers, batch_x)
+        # of shape [n_centers, n_outputs]
+        kg = kernel_mat @ grad
+        eigenvectors = self._eigensys.vectors
+        normalized_ratios = self._eigensys.normalized_ratios
+        
+        # of shape [q, n_outputs]
+        vtkg = eigenvectors.T @ kg
+        vdvtkg = eigenvectors @ (normalized_ratios * vtkg)
+        
+        return vdvtkg
+    
+    def update(self, delta: torch.Tensor, batch_size: int) -> None:
+        """Updates the weight parameters."""
+        lr = self.scaled_learning_rate(batch_size)
+        self._weights.add_(lr * delta)
+        
---
+++ preconditioner_test.py
@@ -0,0 +1,88 @@
+import numpy as np
+import torch
+import unittest
+
+import preconditioner as pcd
+
+
+
+def linear_kernel(x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
+    return x1.matmul(x2.t())
+
+
+class MockEigenSystem:
+    def __init__(self, values, vectors, min_value):
+        self._values = values
+        self._vectors = vectors
+        self._min_value = min_value
+
+
+class TestPreconditionerAndKernelEigenSystem(unittest.TestCase):
+    
+    def test_KernelEigenSystem(self):
+        # Initialize a mock EigenSystem
+        mock_eigensys = MockEigenSystem(
+            values=np.array([3, 2, 0.1]),
+            vectors=np.array([[1, 0], [0, 1]]), min_value=1)
+        
+        # Initialize a KernelEigenSystem based on the mock EigenSystem
+        kernel_eigensys = pcd.KernelEigenSystem(mock_eigensys, beta=10, 
+                                                scale=0.1)
+        
+        # Test beta property
+        self.assertEqual(kernel_eigensys.beta, 10)
+        
+        # Test normalized_ratios property
+        np.testing.assert_array_almost_equal(
+            kernel_eigensys.normalized_ratios,
+            np.array([0.032222, 0.0475]), decimal=6)
+
+    def test_learning_rate(self):
+        centers = torch.tensor([[1, 2, 3], [3, 4, 1], [-1, 0.5, 2.1]],
+                               dtype=torch.float32)
+        weights = torch.tensor([[1.0], [2.0], [3.0]], dtype=torch.float32)
+        top_q_eig = 2
+        precon = pcd.Preconditioner(linear_kernel, centers, weights, top_q_eig)
+        
+        self.assertEqual(precon.critical_batch_size, 92)
+        self.assertAlmostEqual(precon.learning_rate(1), 0.01923, places=4)
+        self.assertAlmostEqual(precon.learning_rate(10), 0.19230, places=4)
+        self.assertAlmostEqual(precon.learning_rate(92), 1.78114, places=4)
+        self.assertAlmostEqual(precon.learning_rate(100), 1.85503, places=4)
+        self.assertAlmostEqual(precon.learning_rate(1000), 3.25088, places=4)
+        self.assertAlmostEqual(precon.learning_rate(10000), 3.51539, places=4)
+        
+    def test_delta(self):
+        centers = torch.tensor([[1, 2, 3, 2],
+                                [3, 4, 1, 1],
+                                [-1, 0.5, 2.1, 0]],
+                               dtype=torch.float32)
+        weights = torch.tensor([[1.0],
+                                [2.0],
+                                [0.0]], dtype=torch.float32)
+        top_q_eig = 1
+        precon = pcd.Preconditioner(linear_kernel, centers, weights, top_q_eig)
+        
+        batch_x = torch.tensor([[1, 2, 0, 0.2],
+                                [3, 4, -1, 0.6]], dtype=torch.float32)
+        grad = torch.tensor([[0.5],
+                             [0.6]], dtype=torch.float32)
+        
+        delta = precon.delta(batch_x, grad)
+        np.testing.assert_array_almost_equal(
+            delta, np.array([[0.079796],
+                             [0.1015  ],
+                             [0.018009]]), decimal=6)
+                
+    # def test_update(self):
+    #     centers = torch.tensor([[1, 2], [3, 4]], dtype=torch.float32)
+    #     weights = torch.tensor([[1.0], [2.0]], dtype=torch.float32)
+    #     top_q_eig = 2
+    #     precon = pcd.Preconditioner(linear_kernel, centers, weights, top_q_eig)
+        
+    #     delta = torch.tensor([[0.1], [0.2]], dtype=torch.float32)
+        
+    #     # Update the weights
+    #     precon.update(delta, 2)
+
+unittest.main(argv=[''], verbosity=2, exit=False)
