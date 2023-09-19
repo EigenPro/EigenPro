2f86392105bcaa4622efa453ee6e845d26938fec
Amirhesam Abedsoltan <amirhesam@exp-7-59.expanse.sdsc.edu>
2023-09-19 15:14:43 -0700

    lru modified

--- models.py
+++ models.py
@@ -4,8 +4,18 @@ from typing import Callable, List, Optional
 import torch
 from concurrent.futures import ThreadPoolExecutor
 from device import Device
-from utils import LRUCache
 
+
+class LRUCache():
+  def __init__(self):
+    self.cache = {}
+
+  def get(self, key: int) -> int:
+    return self.cache.get(key, -1)
+
+  def put(self, key: int, value: int) -> None:
+    self.cache.clear()  # Since capacity is 1, clear the cache before adding a new item
+    self.cache[key] = value
 
 class KernelMachine:
   """Base class for KernelMachine.
--- utils.py
+++ utils.py
@@ -19,16 +19,7 @@ def create_kernelmodel(Z,n_outputs,kernel_fn):
     return ShardedKernelMachine(kms,device)
 
 
-class LRUCache:
-    def __init__(self):
-        self.cache = {}
 
-    def get(self, key: int) -> int:
-        return self.cache.get(key, -1)
-
-    def put(self, key: int, value: int) -> None:
-        self.cache.clear()  # Since capacity is 1, clear the cache before adding a new item
-        self.cache[key] = value
 
 
 
