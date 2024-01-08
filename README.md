c1ca3d694abd8d3f73627b465c55eb69cf5e90eb
Amirhesam Abedsoltan <amirhesam@exp-7-59.expanse.sdsc.edu>
2024-01-07 17:44:55 -0800

    readme updated

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# EigenPro32
-EigenPro3.2 solver to train general kernel models
+## Tutorial: How to Use This Repository
 
-## Docs
-[GitHub To Do](https://docs.google.com/document/d/1bDfaD5LQLpf1ACNmYLdJZCbcnnjKHSM__QwD0Voj1xU/edit?pli=1#heading=h.v6jhaubovxfb)
+1. **Data Preparation**: 
+   In `main.py`, replace `X`, `Y`, `X_val`, and `Y_val` with your dataset.
+
+2. **Eigenpro Configuration**:
+   - **Kernel and Bandwidth**: Choose your preferred kernel and bandwidth in the Eigenpro configs section. If unsure, set `kernel_fn = None` and we'll auto-select for you.
+   - **Precision**: Change `type` to `torch.float32` for higher precision. `float16` is generally adequate for most datasets.
+
+3. **Usage**: 
+   Review the comments in `run_eigenpro` to determine the best usage for your situation.
+
+4. **Execution**: 
+   After editing, run the script with `python main.py`.
--- main.py
+++ main.py
@@ -1,4 +1,122 @@
-from utils import create_kernelmodel
+import numpy as np
+import torch
+import os
+import wandb
+import numpy as np
+import ipdb
+import argparse
+
+from torch.nn.functional import one_hot
+from torch.utils.data import DataLoader
+from device import Device
+from run import run_eigenpro
+from kernels import laplacian
+
+from torchvision import datasets, transforms
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--n", type=int, help="Number of data points",default = 50_000)
+parser.add_argument("--p", type=int, help="Model size",default = 20_000)
+parser.add_argument("--s_data", type=int, help="s_data",default = 2_000)
+parser.add_argument("--s_model", type=int, help="s_model",default = 2_000)
+parser.add_argument("--q_data", type=int, help="q_data",default = 100)
+parser.add_argument("--q_model", type=int, help="q_model",default = 100)
+parser.add_argument("--epochs", type=int, help="q_model",default = 2)
+args = parser.parse_args()
+
+
+##### Loading data set for FASHIONMNIST ########
+data_root = '/expanse/lustre/projects/csd697/amirhesam/data/'
+# Define transformations
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+train_dataset = datasets.FashionMNIST(root=data_root, train=True, download=True, transform=transform)
+test_dataset = datasets.FashionMNIST(root=data_root, train=False, download=True, transform=transform)
+
+n = args.n
+p = args.p
+
+X_all = train_dataset.data.reshape(-1,28*28)/255.0
+Y = one_hot(train_dataset.targets.long())
+train_set_indices = np.random.choice(60_000,n,replace=False)
+X = X_all[train_set_indices,:]
+Y = Y[train_set_indices]
+
+centers_set_indices = np.random.choice(60_000,p,replace=False)
+Z = X_all[centers_set_indices,:]
+X_val = test_dataset.data.reshape(-1,28*28)/255.0
+Y_val = one_hot(test_dataset.targets.long())
+
+
+
+######### Wandb ##########
+##### if you want to use wandb you can use the template below and pass the run to run_eigenpro below
+# wandb_init = {}
+# wandb_init["project_name"] ="---"
+# wandb_init["mode"]  = 'online'
+# wandb_init["key"]  = "----"
+# wandb_init["org"] = "----"
+#
+# os.environ["WANDB_API_KEY"] = wandb_init['key']
+# os.environ["WANDB_MODE"] = wandb_init['mode']  # online or offline
+#
+# run = wandb.init(project=wandb_init['project_name'], \
+#                  entity=wandb_init['org'])
+#
+# run.name = f'---'
+# run.save()
+
+
+######Eigenpro configs
+type = torch.float16
+n_epochs = args.epochs
+s_data, s_model, q_data, q_model = args.s_data,  args.s_model, args.q_data, args.q_model
+bandwidth = 20.0
+kernel_fn = lambda x, z: laplacian(x, z, bandwidth = bandwidth)
+device = Device.create()
+device_base = device.device_base
+
+######### Eigenpro ########
+model = run_eigenpro(Z, X, Y, X_val, Y_val,device,type=type,kernel=kernel_fn,
+             s_data= s_data, s_model= s_model, q_data=q_data, q_model=q_model,
+             tmp_centers_coeff = 2, wandb =  None, epochs=n_epochs)
+
+
+###### IF X=Z which means centers are the same as the whole training set use the following
+# model = run_eigenpro(X, X, Y, X_val, Y_val,device,type=type, kernel=kernel_fn,
+#              s_data= s_data, s_model= s_model, q_data=q_data, q_model=q_model,
+#              tmp_centers_coeff = 2, wandb =  None, epochs=n_epochs, accumulated_gradients =False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 X,Y,Z  = load(dataset)
 loader = Loader(X,y)
