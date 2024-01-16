2204b497f73292c13c10eaaa579c639e1b6822c2
Parthe Pandit <parthe@users.noreply.github.com>
2024-01-16 16:43:03 +0530

    Created setup.py to install directly from github
    I think this only works once we make the repository public

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import eigenpro
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='eigenpro',
+    version=eigenpro.__version__,
+    author='Amirhesam Abedsoltan, Siyuan Ma, Parthe Pandit',
+    author_email='aabedsoltan@ucsd.edu, siyuan.ma9@gmail.com, parthe1292@gmail.com',
+    description='Fast solver for Kernel Regression using GPUs with linear space and time complexity',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/EigenPro/EigenPro',
+    project_urls = {
+        "Bug Tracker": "https://github.com/EigenPro/EigenPro/issues"
+    },
+    license='Apache-2.0 license',
+    packages=find_packages(),
+    install_requires=[],
+)
