ff38b077f9f87d72a0f371fa0b7fa2cfc03caf93
Parthe Pandit <parthe1292@gmail.com>
2024-01-16 13:27:48 +0530

    moved argument parsing to

--- eigenpro/utils/__init__.py
+++ eigenpro/utils/__init__.py
@@ -3,10 +3,18 @@
 import torch
 from .device import Device
 from collections import OrderedDict
+from concurrent.futures import ThreadPoolExecutor
+import argparse
+import ipdb
 
 DEFAULT_DTYPE = torch.float32
 
-
+      
+class MapReduceEngein():
+    def __init__(self, device):
+        self.device = device
+        self.n_devices = len(device.devices)
+        self.base_device = device.devices[0]
 
     def map(self,f,args_done , args_dup = None):
         # duplicate args_dup
@@ -27,3 +35,15 @@ DEFAULT_DTYPE = torch.float32
 
     def reduce(self,outs):
         return [out.to(self.base_device) for out in outs]
+
+
+def parse_cmd_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--n", type=int, help="Number of data points",default=50_000)
+    parser.add_argument("--p", type=int, help="Model size",default=20_000)
+    parser.add_argument("--s_data", type=int, help="s_data",default=2_000)
+    parser.add_argument("--s_model", type=int, help="s_model",default=2_000)
+    parser.add_argument("--q_data", type=int, help="q_data",default=100)
+    parser.add_argument("--q_model", type=int, help="q_model",default=100)
+    parser.add_argument("--epochs", type=int, help="q_model",default=2)
+    return parser.parse_args()    
