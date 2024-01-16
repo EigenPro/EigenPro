fa89ce0eaab28ec110be26516ac1840b76b66e2b
Parthe Pandit <parthe@users.noreply.github.com>
2024-01-16 13:29:42 +0530

    Update __init__.py
    Moved wandb commented text to `utils.py`

--- eigenpro/utils/__init__.py
+++ eigenpro/utils/__init__.py
@@ -47,3 +47,19 @@ def parse_cmd_args():
     parser.add_argument("--q_model", type=int, help="q_model",default=100)
     parser.add_argument("--epochs", type=int, help="q_model",default=2)
     return parser.parse_args()    
+
+
+
+####################################################################
+# wandb_init = {}
+# wandb_init["project_name"] ="---"
+# wandb_init["mode"]  = 'online'
+# wandb_init["key"]  = "----"
+# wandb_init["org"] = "----"
+# os.environ["WANDB_API_KEY"] = wandb_init['key']
+# os.environ["WANDB_MODE"] = wandb_init['mode']  # online or offline
+# run = wandb.init(project=wandb_init['project_name'], \
+#                  entity=wandb_init['org'])
+# run.name = f'---'
+# run.save()
+####################################################################
