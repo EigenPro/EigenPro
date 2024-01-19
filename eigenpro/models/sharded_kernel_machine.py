15a3407dba533a892c770d2d40fc099138070f6d
Parthe Pandit <parthe@users.noreply.github.com>
2024-01-19 10:15:13 +0530

    Update sharded_kernel_machine.py

--- eigenpro/models/sharded_kernel_machine.py
+++ eigenpro/models/sharded_kernel_machine.py
@@ -1,7 +1,7 @@
 import torch, numpy as np
 from .base import KernelMachine
 from typing import Callable, List, Optional
-from .preallocated_kernel_machine import PreallocatedKernelMachine_optimized
+from .preallocated_kernel_machine import PreallocatedKernelMachine
 from ..utils.cache import LRUCache
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
