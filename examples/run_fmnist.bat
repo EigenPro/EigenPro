0fab65224393d74be660885807d173a5d2c4888e
Siyuan Ma <siyuan.ma9@gmail.com>
2024-06-05 12:43:43 -0700

    Update run_fmnist.bat

--- examples/run_fmnist.bat
+++ examples/run_fmnist.bat
@@ -2,5 +2,17 @@
 REM Get the directory path of the script
 set "script_dir=%~dp0"
 
+REM Remove the trailing backslash from script_dir
+set "script_dir=%script_dir:~0,-1%"
+
+REM Get the parent directory of script_dir
+for %%i in ("%script_dir%") do set "parent_dir=%%~dpi"
+
+REM Remove the trailing backslash from parent_dir
+set "parent_dir=%parent_dir:~0,-1%"
+
+REM Add the parent directory to PYTHONPATH
+set "PYTHONPATH=%parent_dir%;%PYTHONPATH%"
+
 REM Run the Python script
-python "%script_dir%run_example.py" --dataset=fmnist
+python "%script_dir%\run_example.py" --dataset=fmnist
