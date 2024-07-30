ad6944d6d86b944c8ba8d0bab703cdf858ceb6d2
Amirhesam Abedsoltan <91305454+ahabedsoltan@users.noreply.github.com>
2024-07-30 13:44:25 -0700

    Update sharded_kernel_machine.py

--- eigenpro/models/sharded_kernel_machine.py
+++ eigenpro/models/sharded_kernel_machine.py
@@ -11,197 +11,196 @@ import eigenpro.utils.device as device
 
 
 class ShardedKernelMachine(km.KernelMachine):
-    """Kernel machine that shards its computation across multiple devices."""
+	"""Kernel machine that shards its computation across multiple devices."""
+	
+	def __init__(self, kms: List[km.KernelMachine], device: device.Device):
+		self.device = device
+		self.shard_kms = kms
+		self.n_devices = len(kms)
+		self.n_machines = len(kms)
+		self.lru = cache.LRUCache()
+		super().__init__(
+				kms[0].kernel_fn,
+				kms[0].n_outputs,
+				sum(km.size for km in kms)
+		)
+  @property
+  def centers(self):
+      """Return the centers"""
+      return [km.centers for km in self.shard_kms]
 
-    def __init__(self, kms: List[km.KernelMachine], device: device.Device):
-        self.device = device
-        self.shard_kms = kms
-        self.n_devices = len(kms)
-        self.n_machines = len(kms)
-        self.lru = cache.LRUCache()
-        super().__init__(
-            kms[0].kernel_fn,
-            kms[0].n_outputs,
-            sum(km.size for km in kms)
-        )
+  @property
+  def weights(self):
+      """Return the weights"""
+      return [km.weights for km in self.shard_kms]
 
-    @property
-    def centers(self):
-        """Return the centers"""
-        return [km.centers for km in self.shard_kms]
+  def forward(self, x: torch.Tensor, train=True) -> torch.Tensor:
+      """Forward pass for the kernel machine.
 
-    @property
-    def weights(self):
-        """Return the weights"""
-        return [km.weights for km in self.shard_kms]
+      Args:
+          x (torch.Tensor): input tensor of shape [n_samples, n_features].
 
-    def forward(self, x: torch.Tensor, train=True) -> torch.Tensor:
-        """Forward pass for the kernel machine.
+          train(bool): Train mode, storing kernel_mat[:, :self.original_size].T in cache.
 
-        Args:
-            x (torch.Tensor): input tensor of shape [n_samples, n_features].
+      Returns:
+          torch.Tensor: tensor of shape [n_samples, n_outputs].
+      """
 
-            train(bool): Train mode, storing kernel_mat[:, :self.original_size].T in cache.
+      x_broadcast = self.device(x)
+      with ThreadPoolExecutor() as executor:
+          predictions = [executor.submit(self.shard_kms[i].forward, x_broadcast[i], train=train)
+                         for i in range(self.n_devices)]
+      results = [k.result() for k in predictions]
 
-        Returns:
-            torch.Tensor: tensor of shape [n_samples, n_outputs].
-        """
+      p_all = 0
+      k_centers_batch_all = []
+      for i, r in enumerate(results):
+          p_all = p_all + r.to(self.device.device_base)
+          k_centers_batch_all.append(self.shard_kms[i].lru.get('k_centers_batch'))
+          self.shard_kms[i].lru.cache.clear()
+          torch.cuda.empty_cache()
+          del r
 
-        x_broadcast = self.device(x)
-        with ThreadPoolExecutor() as executor:
-            predictions = [executor.submit(self.shard_kms[i].forward, x_broadcast[i], train=train)
-                           for i in range(self.n_devices)]
-        results = [k.result() for k in predictions]
+      if train:
+          self.lru.put('k_centers_batch', k_centers_batch_all)
 
-        p_all = 0
-        k_centers_batch_all = []
-        for i, r in enumerate(results):
-            p_all = p_all + r.to(self.device.device_base)
-            k_centers_batch_all.append(self.shard_kms[i].lru.get('k_centers_batch'))
-            self.shard_kms[i].lru.cache.clear()
-            torch.cuda.empty_cache()
-            del r
+      del x_broadcast, k_centers_batch_all, results, x
+      torch.cuda.empty_cache()
 
-        if train:
-            self.lru.put('k_centers_batch', k_centers_batch_all)
+      return p_all
 
-        del x_broadcast, k_centers_batch_all, results, x
-        torch.cuda.empty_cache()
+  def init_nystorm(self, centers):
 
-        return p_all
+      centers_gpus_list = self.device(centers, strategy="divide_to_gpu")
 
-    def init_nystorm(self, centers):
+      with ThreadPoolExecutor() as executor:
+          outputs = [executor.submit(self.shard_kms[i].init_nystorm, centers_gpus_list[i])
+                     for i in range(self.n_devices)]
 
-        centers_gpus_list = self.device(centers, strategy="divide_to_gpu")
+      for outputs in as_completed(outputs):
+          try:
+              # Retrieve the result of the future
+              _ = outputs.result()
+              # Process the result if necessary
+          except Exception as exc:
+              # Handle exceptions from within the thread
+              print(f"A thread caused an error: {exc}")
+              raise  # Reraising the exception will stop the program
 
-        with ThreadPoolExecutor() as executor:
-            outputs = [executor.submit(self.shard_kms[i].init_nystorm, centers_gpus_list[i])
-                       for i in range(self.n_devices)]
+  def add_centers(self, centers: torch.Tensor, weights: Optional[torch.Tensor] = None) -> None:
+      """Adds new centers and weights.
 
-        for outputs in as_completed(outputs):
-            try:
-                # Retrieve the result of the future
-                _ = outputs.result()
-                # Process the result if necessary
-            except Exception as exc:
-                # Handle exceptions from within the thread
-                print(f"A thread caused an error: {exc}")
-                raise  # Reraising the exception will stop the program
+      Args:
+        centers: Kernel centers of shape [n_centers, n_features].
+        weights: Weight parameters corresponding to the centers of shape
+                 [n_centers, n_output].
 
-    def add_centers(self, centers: torch.Tensor, weights: Optional[torch.Tensor] = None) -> None:
-        """Adds new centers and weights.
+      Returns:
+        center_weights: Weight parameters corresponding to the added centers.
+      """
+      centers_gpus_list = self.device(centers, strategy="divide_to_gpu")
+      center_weights = weights if weights is not None else torch.zeros(
+          (centers.shape[0], self.n_outputs))
+      weights_gpus_list = self.device(center_weights, strategy="divide_to_gpu")
 
-        Args:
-          centers: Kernel centers of shape [n_centers, n_features].
-          weights: Weight parameters corresponding to the centers of shape
-                   [n_centers, n_output].
+      with ThreadPoolExecutor() as executor:
+          outputs = [executor.submit(self.shard_kms[i].add_centers, centers_gpus_list[i]
+                                     , weights_gpus_list[i]) for i in range(self.n_devices)]
 
-        Returns:
-          center_weights: Weight parameters corresponding to the added centers.
-        """
-        centers_gpus_list = self.device(centers, strategy="divide_to_gpu")
-        center_weights = weights if weights is not None else torch.zeros(
-            (centers.shape[0], self.n_outputs))
-        weights_gpus_list = self.device(center_weights, strategy="divide_to_gpu")
+      for outputs in as_completed(outputs):
+          try:
+              # Retrieve the result of the future
+              _ = outputs.result()
+              # Process the result if necessary
+          except Exception as exc:
+              # Handle exceptions from within the thread
+              print(f"A thread caused an error: {exc}")
+              raise  # Reraising the exception will stop the program
 
-        with ThreadPoolExecutor() as executor:
-            outputs = [executor.submit(self.shard_kms[i].add_centers, centers_gpus_list[i]
-                                       , weights_gpus_list[i]) for i in range(self.n_devices)]
+      return center_weights
 
-        for outputs in as_completed(outputs):
-            try:
-                # Retrieve the result of the future
-                _ = outputs.result()
-                # Process the result if necessary
-            except Exception as exc:
-                # Handle exceptions from within the thread
-                print(f"A thread caused an error: {exc}")
-                raise  # Reraising the exception will stop the program
+  def update_by_index(self, indices: torch.Tensor,
+              delta: torch.Tensor) -> None:
+      """Update the model weights by index.
 
-        return center_weights
+      Here we assume that only the first block is trainable.
 
-    def update_by_index(self, indices: torch.Tensor,
-                delta: torch.Tensor) -> None:
-        """Update the model weights by index.
+      Args:
+        indices: Tensor of 1-D indices to select rows of weights.
+        delta: Tensor of weight update of shape [n_indices, n_outputs].
+        nystrom_update: updating data nystrom samples in the temporory centers phase.
 
-        Here we assume that only the first block is trainable.
+      """
+      indices_list = []
+      delta_list = []
+      threshold_now = 0
+      threshold_before = 0
 
-        Args:
-          indices: Tensor of 1-D indices to select rows of weights.
-          delta: Tensor of weight update of shape [n_indices, n_outputs].
-          nystrom_update: updating data nystrom samples in the temporory centers phase.
+      for i in range(self.n_devices):
 
-        """
-        indices_list = []
-        delta_list = []
-        threshold_now = 0
-        threshold_before = 0
+          threshold_now = threshold_now + self.shard_kms[i].original_size
+          gp1_indices = np.where((indices < threshold_now) & (indices >= threshold_before))[0]
+          indices_in_gpui = indices[gp1_indices] - threshold_before
+          indices_list.append(indices_in_gpui)
+          delta_list.append(delta[gp1_indices])
+          threshold_before = threshold_now
 
-        for i in range(self.n_devices):
+      with ThreadPoolExecutor() as executor:
+          _ = [executor.submit(self.shard_kms[i].update_by_index, indices_list[i],
+                               delta_list[i]) for i in range(self.n_devices)]
 
-            threshold_now = threshold_now + self.shard_kms[i].original_size
-            gp1_indices = np.where((indices < threshold_now) & (indices >= threshold_before))[0]
-            indices_in_gpui = indices[gp1_indices] - threshold_before
-            indices_list.append(indices_in_gpui)
-            delta_list.append(delta[gp1_indices])
-            threshold_before = threshold_now
-
-        with ThreadPoolExecutor() as executor:
-            _ = [executor.submit(self.shard_kms[i].update_by_index, indices_list[i],
-                                 delta_list[i]) for i in range(self.n_devices)]
-
-    def reset(self):
-        """reset the model to before adding temporary centers were added adn before projection
-        Args:
-            No arguments.
-        Returns:
-            None
-        """
-        with ThreadPoolExecutor() as executor:
-            [executor.submit(self.shard_kms[i].reset())
-             for i in range(self.n_devices)]
+  def reset(self):
+      """reset the model to before adding temporary centers were added adn before projection
+      Args:
+          No arguments.
+      Returns:
+          None
+      """
+      with ThreadPoolExecutor() as executor:
+          [executor.submit(self.shard_kms[i].reset())
+           for i in range(self.n_devices)]
 
 
 def create_sharded_kernel_machine(centers: torch.Tensor, n_outputs: int, kernel_fn: Callable,
-                                  device: device.Device, dtype: torch.dtype = torch.float32,
-                                  tmp_centers_coeff: int = 2) -> ShardedKernelMachine:
-    """Creates a sharded kernel machine for distributed computation.
+                                device: device.Device, dtype: torch.dtype = torch.float32,
+                                tmp_centers_coeff: int = 2) -> ShardedKernelMachine:
+  """Creates a sharded kernel machine for distributed computation.
 
-    This function divides the input centers among available devices and initializes
-    a list of PreallocatedKernelMachines, each on a different device. It then creates
-    and returns a ShardedKernelMachine that manages these distributed kernel machines.
+  This function divides the input centers among available devices and initializes
+  a list of PreallocatedKernelMachines, each on a different device. It then creates
+  and returns a ShardedKernelMachine that manages these distributed kernel machines.
 
-    Args:
-        centers: The tensor of centers used for the kernel machines, with shape [n_centers, n_features].
-        n_outputs: The number of outputs for the kernel machine.
-        kernel_fn: The kernel function to be used in the kernel machine.
-        device: The device (or device manager) handling distribution of computation.
-        dtype: The data type for computation, default is torch.float32.
-        tmp_centers_coeff: Coefficient to determine the number of temporary centers, default is 2.
+  Args:
+      centers: The tensor of centers used for the kernel machines, with shape [n_centers, n_features].
+      n_outputs: The number of outputs for the kernel machine.
+      kernel_fn: The kernel function to be used in the kernel machine.
+      device: The device (or device manager) handling distribution of computation.
+      dtype: The data type for computation, default is torch.float32.
+      tmp_centers_coeff: Coefficient to determine the number of temporary centers, default is 2.
 
-    Returns:
-        A ShardedKernelMachine instance that manages the distributed kernel machines.
+  Returns:
+      A ShardedKernelMachine instance that manages the distributed kernel machines.
 
-    """
-    # Divide the centers among available GPUs as per the provided strategy
-    list_of_centers = device(centers, strategy="divide_to_gpu")
+  """
+  # Divide the centers among available GPUs as per the provided strategy
+  list_of_centers = device(centers, strategy="divide_to_gpu")
 
-    # Initialize kernel machines for each set of divided centers
-    kms: List[pkm.PreallocatedKernelMachine] = []
-    for i, centers_i in enumerate(list_of_centers):
-        kms.append(
-            pkm.PreallocatedKernelMachine(
-                kernel_fn=kernel_fn,
-                n_outputs=n_outputs,
-                centers=centers_i,
-                dtype=dtype,
-                device=device.devices[i],
-                tmp_centers_coeff=tmp_centers_coeff
-            )
-        )
+  # Initialize kernel machines for each set of divided centers
+  kms: List[pkm.PreallocatedKernelMachine] = []
+  for i, centers_i in enumerate(list_of_centers):
+      kms.append(
+          pkm.PreallocatedKernelMachine(
+              kernel_fn=kernel_fn,
+              n_outputs=n_outputs,
+              centers=centers_i,
+              dtype=dtype,
+              device=device.devices[i],
+              tmp_centers_coeff=tmp_centers_coeff
+          )
+      )
 
-    # Clear the list to potentially free up memory
-    del list_of_centers
+  # Clear the list to potentially free up memory
+  del list_of_centers
 
-    # Create and return the sharded kernel machine with the initialized kernel machines
-    return ShardedKernelMachine(kms, device)
+  # Create and return the sharded kernel machine with the initialized kernel machines
+  return ShardedKernelMachine(kms, device)
